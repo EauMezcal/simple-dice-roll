cf0cef74e3ef462a2d6bc463ea8510abdff46c10
EauMezcal <eauxi_123@163.com>
2022-07-04 21:08:07 +0800

    GUI布局完善

--- roller.py
+++ roller.py
@@ -1,35 +1,40 @@
 import tkinter as tk
 import random
-from webbrowser import BackgroundBrowser
-
 
 class Roller(object):
     def __init__(self):
         self.root=tk.Tk()
         # 标题
-        self.root.title("Roller!")
-        # self.root.geometry('450X300')
-        # self.config(Background="write")
-        self.D_input = tk.Entry(self.root, width=30)
+        self.root.title("Simple-Dice-Roller!")
+        self.root.geometry('450x500+100+100')
+        # 输入框
+        self.D_input = tk.Entry(self.root, width= 15)
         # 回显列表
-        self.display_info = tk.Listbox(self.root, width=50)
-        self.Roll_Button = tk.Button(self.root, command= self.roll, text = "roll!!")
-        # self.display_info = tk.Listbox(self.root, width=50)
-
-        tk.Radiobutton(self, text="1D 100", variable=self.v, value=3).pack(anchor = 'w')
+        self.display_info = tk.Listbox(self.root, width= 30, height= 25)
+        self.text1 = tk.Label(self.root, text="1D ? :")
+        self.Roll_Button = tk.Button(self.root, command= self.roll, text = "roll", width=7)
+        self.clean_Button = tk.Button(self.root,command= self.clean, text= "Clean" ,bg="#BA3E35",fg="#ffffff")
         self.site = [('1D 2',1),
-                    ('1D 6',2),
-                    ('1D 10',3),
-                    ('1D 100',4)]
+                     ('1D 6',2),
+                     ('1D 10',3),
+                     ('1D 100',4)]
         self.v = tk.IntVar()
         for name, num in self.site:
-            radio_button = tk.Radiobutton(text = name, variable = self.v,value =num,command=self.roll_choose,indicatoron=False)
-            radio_button.pack(anchor ='w')
-    def gui(self):
-        self.D_input.pack()
-        self.display_info.pack()
-        self.Roll_Button.pack()
-
+            radio_button = tk.Radiobutton(text = name, variable = self.v,value =num,command=self.roll_choose,indicatoron=False, width=7)
+            radio_button.place(x=10,y=10+(num-1)*30)
+        
+        # GUI
+        
+        # 文本框
+        self.D_input.place(x=10,y=160)
+        # 回显框
+        self.display_info.place(x=200,y=10)
+        # roll按钮
+        self.Roll_Button.place(x=10,y=190)
+        # clean按钮
+        self.clean_Button.place(x=150,y=10)
+        # 文本
+        self.text1.place(x=10,y=135)
 
     def roll_choose(self):
         self.dict={1:'2',2:'6',3:'10',4:'100'}
@@ -43,24 +48,17 @@ class Roller(object):
         self.D = int(self.D_input.get())
         roll = random.randint(1,self.D)
         the_roll_info = ["1D %d roll:" % int(self.D_input.get()) + str(roll)]
-
-        # for item in range(10):
-        #     self.display_info.insert(0,"")
         
         for item in the_roll_info:
             self.display_info.insert(0,item)
-    # def Roll_1D2:
+    def clean(self):    
+        self.display_info.delete(0,9999)
         
 def main():
     A = Roller()
-    A.gui()
+    # A.gui()
     tk.mainloop()
     pass
 
 if __name__=="__main__":
     main()
-# while True:
-#     s = input('1D  ')
-#     s = int(s)
-#     a = random.randint(1,s)
-#     print('结果：'+str(a))
